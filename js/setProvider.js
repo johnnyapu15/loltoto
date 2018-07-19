a4e73210ee7622ee12910591d908cc2fc4ac37e6
Jeong Juahn <johnnyapu15@gmail.com>
2018-07-19 20:52:00 +0900

    cm9

--- js/setProvider.js
+++ js/setProvider.js
@@ -2,11 +2,17 @@ var contractAddress = '0xb20083039a3b7b76c0dc3884c6e5f41c3784671d';
 var abi = [{"constant":false,"inputs":[{"name":"_receiver","type":"address"},{"name":"_message","type":"string"}],"name":"sendMsg","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getMsg","outputs":[{"name":"","type":"uint256"},{"name":"","type":"address"},{"name":"","type":"string"},{"name":"","type":"bool"}],"payable":false,"type":"function"}];
 
 window.isProvided = false;
+window.account = 0;
+
 window.addEventListener('load', function(){
     console.log("gg " + window.isProvided);
     if (typeof web3 !== 'undefined'){
         window.web3 = new Web3(web3.currentProvider);
-        window.isProvided = true;
+        if (web3.eth.accounts[0] !== window.account){
+            window.account = web3.eth.accounts[0];
+            window.isProvided = true;
+            alert("login!");
+        }
     }
     else {
         window.isProvided = false;
