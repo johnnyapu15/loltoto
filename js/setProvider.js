cd0c1c9c8368802cd85ab2da2346e5f57c22a94a
Jeong Juahn <johnnyapu15@gmail.com>
2018-07-19 21:27:58 +0900

    cm16

--- js/setProvider.js
+++ js/setProvider.js
@@ -2,6 +2,7 @@ var contractAddress = '0xb20083039a3b7b76c0dc3884c6e5f41c3784671d';
 var abi = [{"constant":false,"inputs":[{"name":"_receiver","type":"address"},{"name":"_message","type":"string"}],"name":"sendMsg","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getMsg","outputs":[{"name":"","type":"uint256"},{"name":"","type":"address"},{"name":"","type":"string"},{"name":"","type":"bool"}],"payable":false,"type":"function"}];
 var loginBoolean = false;
 window.isProvided = false;
+window.account = undefined;
 
 
 window.addEventListener('load', function(){
@@ -23,7 +24,7 @@ setInterval(function(){
                 window.account = web3.eth.accounts[0];
                 window.isProvided = true;
             }
-            else {
+            else if (window.account !== web3.eth.accounts[0]){
                 //Logout. Need to reload page.
                 window.location.reload();
                 window.account = web3.eth.accounts[0];
