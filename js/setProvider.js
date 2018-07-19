d3f2b48ba9012bad7bde0a219b2d7d2d5844b166
Jeong Juahn <johnnyapu15@gmail.com>
2018-07-19 20:57:50 +0900

    cm9

--- js/setProvider.js
+++ js/setProvider.js
@@ -1,20 +1,30 @@
 var contractAddress = '0xb20083039a3b7b76c0dc3884c6e5f41c3784671d';
 var abi = [{"constant":false,"inputs":[{"name":"_receiver","type":"address"},{"name":"_message","type":"string"}],"name":"sendMsg","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getMsg","outputs":[{"name":"","type":"uint256"},{"name":"","type":"address"},{"name":"","type":"string"},{"name":"","type":"bool"}],"payable":false,"type":"function"}];
-
+var loginBoolean = false;
 window.isProvided = false;
-window.account = 0;
+
 
 window.addEventListener('load', function(){
     console.log("gg " + window.isProvided);
     if (typeof web3 !== 'undefined'){
         window.web3 = new Web3(web3.currentProvider);
+        window.account = web3.eth.accounts[0];
+        loginBoolean = true;
+    }
+    else {
+       
+    }
+})
+
+setInterval(function(){
+    if (loginBoolean){
         if (web3.eth.accounts[0] !== window.account){
             window.account = web3.eth.accounts[0];
             window.isProvided = true;
             alert("login!");
         }
+        else {
+            window.isProvided = false;
+        }
     }
-    else {
-        window.isProvided = false;
-    }
-})
+}, 1000)
