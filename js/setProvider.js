4560a52766af173f53bfbc4997f885c649ff977d
Jeong Juahn <johnnyapu15@gmail.com>
2018-07-19 19:53:33 +0900

    cm6

--- js/setProvider.js
+++ js/setProvider.js
@@ -1,5 +1,3 @@
-
-
 var contractAddress = '0xb20083039a3b7b76c0dc3884c6e5f41c3784671d';
 abi = [{"constant":false,"inputs":[{"name":"_receiver","type":"address"},{"name":"_message","type":"string"}],"name":"sendMsg","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getMsg","outputs":[{"name":"","type":"uint256"},{"name":"","type":"address"},{"name":"","type":"string"},{"name":"","type":"bool"}],"payable":false,"type":"function"}];
 
